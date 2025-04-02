bc45570bdfe5ea0e17aa895a87d98b5877f18ebd
kishor <kishorebsc12@gmail.com>
2025-04-02 16:00:50 +0530

    tool

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  backend "s3" {
+    bucket = "remote-file-kishor"
+    key = "tools/state"
+    region = "us-east-1"
+  }
+}
+
+variable "ami_id" {
+    default = "ami-09c813fb71547fc4f"
+}
+variable "instance_type" {}
+variable "zone_id" {
+  default = "Z10310253KPZLFJOC7YEK"
+}
+
+variable "tools" {
+  default = {
+    vault ={
+        instance_type = "t3.small"
+        port = 8200
+    }
+  }
+}
+
+
+module "tool_infra" {
+  source = "./module-infra"
+  for_each = var.tools
+
+  ami_id = var.ami_id
+  instance_type = each.value["instance_type"]
+  name = each.key
+  zone_id = var.zone_id
+  port = each.value["port"]
+
+}
