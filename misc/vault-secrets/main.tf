90b7b20082a27e1d09c2a92c4b67467db331872d
kishor <kishorebsc12@gmail.com>
2025-04-03 15:20:06 +0530

    added

---
+++ misc/vault-secrets/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket = "remote-file-kishor"
+    key = "vault-secret/state"
+    region = "us-east-1"
+  }
+}
+
+provider "vault" {
+  address = "http://vault-internal.kommanuthala.store:8200"
+  token = var.vault_token
+}
+
+variable "vault_token" {}
+
+resource "vault_generic_secret" "ssh" {
+  path="infra/ssh"
+  data_json = <<EOT
+  {
+  "username" = "ec2-user"
+  "password" = "DevOps321"
+  }
+  EOT
+}
